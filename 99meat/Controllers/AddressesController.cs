8c4f6f48f3a9ab101a22a291bec75f11b6013f23
abhi <abhinav.moolagundla@gmail.com>
2016-12-07 19:08:56 -0600

    new

---
+++ 99meat/Controllers/AddressesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using _99meat.Models;
+
+namespace _99meat.Controllers
+{
+    [Authorize]
+   
+    public class AddressesController : ApiController
+    {
+         
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Addresses
+        public IQueryable<Address> GetAddresses()
+        {
+            return db.Addresses;
+        }
+
+        // GET: api/Addresses/5
+        [ResponseType(typeof(Address))]
+        public async Task<IHttpActionResult> GetAddress(int id)
+        {
+            Address address = await db.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(address);
+        }
+
+        // GET: api/Addresses/5
+        [ResponseType(typeof(Address))]
+        [Route("api/Addresses/GetByUserName/{userName}")]
+        public IQueryable<Address> GetByUserName(string userName)
+        {
+            IQueryable<Address>  address =  db.Addresses;
+
+            if (address == null)
+            {
+                return null;
+            }
+
+            return (IQueryable<Address>)address.ToList().FindAll(x => x.UserName.ToLower().ToString().Equals(userName));
+        }
+
+
+
+
+        // PUT: api/Addresses/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutAddress(int id, Address address)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != address.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(address).State = System.Data.Entity.EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Addresses
+        [ResponseType(typeof(Address))]
+        public async Task<IHttpActionResult> PostAddress(Address address)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Addresses.Add(address);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = address.Id }, address);
+        }
+
+        // DELETE: api/Addresses/5
+        [ResponseType(typeof(Address))]
+        public async Task<IHttpActionResult> DeleteAddress(int id)
+        {
+            Address address = await db.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            db.Addresses.Remove(address);
+            await db.SaveChangesAsync();
+
+            return Ok(address);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AddressExists(int id)
+        {
+            return db.Addresses.Count(e => e.Id == id) > 0;
+        }
+    }
+}

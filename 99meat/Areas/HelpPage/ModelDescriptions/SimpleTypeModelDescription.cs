f792745d13164a1980abea4df5edaa11c070a93d
abhi <abhinav.moolagundla@gmail.com>
2016-12-07 19:00:48 -0600

    inital commit

---
+++ 99meat/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _99meat.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ 99meat/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace _99meat
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_BeginRequest()
+        {
+            
+                Request.Headers.Remove("Access-Control-Allow-Origin");
+                Request.Headers.Add("Access-Control-Allow-Origin", "*");
+                Response.Headers.Remove("Access-Control-Allow-Origin");
+                  Response.Headers.Add("Access-Control-Allow-Origin", "*");
+           
+        }
+
+        protected void Application_EndRequest()
+        {
+                Request.Headers.Remove("Access-Control-Allow-Origin");
+                Request.Headers.Add("Access-Control-Allow-Origin", "*");
+                Response.Headers.Remove("Access-Control-Allow-Origin");
+                Response.Headers.Add("Access-Control-Allow-Origin", "*");
+          
+        }
+    }
+}

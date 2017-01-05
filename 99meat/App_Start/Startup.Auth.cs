7b218c5d175dbfedba1df02243f5ff1bf412630c
abhi <abhinav.moolagundla@gmail.com>
2017-01-04 20:45:53 -0600

    JWT tokens

--- 99meat/App_Start/Startup.Auth.cs
+++ 99meat/App_Start/Startup.Auth.cs
@@ -10,6 +10,9 @@ using Microsoft.Owin.Security.OAuth;
 using Owin;
 using _99meat.Providers;
 using _99meat.Models;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security;
 
 namespace _99meat
 {
@@ -25,7 +28,7 @@ namespace _99meat
             // Configure the db context and user manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-
+            ConfigureOAuth(app);
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
@@ -35,17 +38,16 @@ namespace _99meat
             PublicClientId = "self";
             OAuthOptions = new OAuthAuthorizationServerOptions
             {
-                TokenEndpointPath = new PathString("/Token"),
-                Provider = new ApplicationOAuthProvider(PublicClientId),
-                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
-                // In production mode set AllowInsecureHttp = false
-                AllowInsecureHttp = true
+                //For Dev enviroment only (on production should be AllowInsecureHttp = false)
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth2/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://99pumba.azurewebsites.net")
             };
-
-            // Enable the application to use bearer tokens to authenticate users
-            app.UseOAuthBearerTokens(OAuthOptions);
-
+          
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(OAuthOptions);
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
             //    clientId: "",
@@ -59,11 +61,31 @@ namespace _99meat
                 appId: "172669669872080",
                 appSecret: "f0135644bc58bd11722218a9411dcaf3");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "406482357619-q3bg29mevtkikt5c8jrp2lb32cmhakfd.apps.googleusercontent.com",
+                ClientSecret = "btO4W41qKuUvYsJqzAERleFI"
+            });
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            var issuer = "http://99pumba.azurewebsites.net";
+            var audience = "099153c2625149bc8ecb3e85e03f0022";
+            var secret = TextEncodings.Base64Url.Decode("IxrAjDoa2FqElO7IhrSrUJELhUckePEPVpaePlS_Xaw");
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audience },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                    }
+                });
+
         }
     }
 }
--- 99meat/Models/AccountBindingModels.cs
+++ 99meat/Models/AccountBindingModels.cs
@@ -7,7 +7,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace _99meat.Models
 {
     // Models used as parameters to AccountController actions.
+    public class Audience
+    {
+        [Key]
+        [MaxLength(32)]
+        public string ClientId { get; set; }
 
+        [MaxLength(80)]
+        [Required]
+        public string Base64Secret { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+    }
+
+    public class AudienceModel
+    {
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+    }
     public class AddExternalLoginBindingModel
     {
         [Required]
